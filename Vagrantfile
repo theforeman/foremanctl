9a0bb3d66ecc0453e7db11bd2070e455b3d7e152
Eric D. Helms <ericdhelms@gmail.com>
2024-03-16 22:08:02 -0400

    Add more intial files

---
+++ Vagrantfile
@@ -0,0 +1,16 @@
+CENTOS_8_BOX_URL = "https://cloud.centos.org/centos/8-stream/x86_64/images/CentOS-Stream-Vagrant-8-20220913.0.x86_64.vagrant-libvirt.box"
+CENTOS_9_BOX_URL = "https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-Vagrant-9-latest.x86_64.vagrant-libvirt.box"
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "centos/stream9"
+  config.vm.synced_folder ".", "/vagrant"
+
+  config.vm.define "quadlet" do |override|
+    override.vm.hostname = "quadlet.example.com"
+
+    override.vm.provider "libvirt" do |libvirt, provider|
+      libvirt.memory = "4096"
+      provider.vm.box_url = CENTOS_9_BOX_URL
+    end
+  end
+end
---
+++ vagrant.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+# this is a very crude hack and can be removed once we have no more python2 systems
+# that need to run this code
+"exec" "$(command -v python3 || command -v python)" "$0" "$@"
+# Adapted from Mark Mandel's implementation
+# https://github.com/ansible/ansible/blob/devel/plugins/inventory/vagrant.py
+import argparse
+import json
+import os
+import subprocess
+import sys
+
+try:
+    from StringIO import StringIO  # pyright: reportMissingImports=false
+except ImportError:
+    from io import StringIO  # pyright: reportMissingImports=false
+
+from collections import defaultdict
+
+
+try:
+    DEVNULL = subprocess.DEVNULL
+except AttributeError:
+    DEVNULL = open(os.devnull, 'w')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Vagrant inventory script")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--list', action='store_true')
+    group.add_argument('--host')
+    return parser.parse_args()
+
+
+def get_running_hosts():
+    try:
+        subprocess.check_call(["which", "vagrant"], stdout=DEVNULL)
+    except subprocess.CalledProcessError:
+        return
+
+    cmd = "vagrant status --machine-readable"
+    status = subprocess.check_output(cmd.split(), universal_newlines=True).rstrip()
+
+    for line in status.split('\n'):
+        if len(line.split(',')) == 4:
+            (_, host, key, value) = line.split(',')
+        else:
+            (_, host, key, value, _) = line.split(',')
+
+        if key == 'state' and value in ('active', 'running'):
+            yield host
+
+
+def list_running_hosts():
+    hosts = list(get_running_hosts())
+    variables = dict(get_configs(hosts))
+
+    return {
+        "_meta": {
+            "hostvars": variables,
+        },
+        "all": {
+            "hosts": hosts,
+        },
+    }
+
+
+def get_ssh_configs(hosts):
+    cmd = ['vagrant', 'ssh-config'] + hosts
+    try:
+        output = subprocess.check_output(cmd, universal_newlines=True, stderr=DEVNULL)
+    except subprocess.CalledProcessError:
+        return None
+
+    config = defaultdict(dict)
+    host = None
+
+    for line in output.splitlines():
+        line = line.strip()
+        if not line:
+            continue
+        key, value = line.split(None, 1)
+        if key == 'Host':
+            host = value
+        elif host:
+            config[host][key.lower()] = value
+
+    return config
+
+
+def get_host_ssh_config(config, host):
+    ssh = config.get(host)
+    return {'ansible_host': ssh['hostname'],
+            'ansible_port': ssh['port'],
+            'ansible_user': ssh['user'],
+            'ansible_ssh_private_key_file': ssh['identityfile']}
+
+
+def get_configs(hosts):
+    if not hosts:
+        return
+
+    ssh_configs = get_ssh_configs(hosts)
+
+    for host in hosts:
+        details = {}
+        if ssh_configs:
+            details.update(get_host_ssh_config(ssh_configs, host))
+        yield host, details
+
+
+def main():
+    args = parse_args()
+    if args.list:
+        hosts = list_running_hosts()
+        json.dump(hosts, sys.stdout)
+    elif args.host:
+        details = dict(get_configs([args.host]))
+        json.dump(details[args.host], sys.stdout)
+
+
+if __name__ == '__main__':
+    main()
