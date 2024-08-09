27af6f17917e2f963c89fc435c4ccb1fa4ea6968
Eric D. Helms <ericdhelms@gmail.com>
2024-08-09 18:46:05 -0400

    Tests are passing

--- tests/candlepin_test.py
+++ tests/candlepin_test.py
@@ -1,15 +1,14 @@
 def test_candlepin_service(host):
     candlepin = host.service("candlepin")
-    assert candlepin.exists
     assert candlepin.is_running
     assert candlepin.is_enabled
 
 
 def test_candlepin_port(host):
     candlepin = host.addr("localhost")
-    assert candlepin.port("8443").is_reachable
+    assert candlepin.port("23443").is_reachable
 
 
 def test_candlepin_status(host):
-    status = host.run('curl -k -s -o /dev/null -w \'%{http_code}\' https://localhost:8443/candlepin/status')
+    status = host.run('curl -k -s -o /dev/null -w \'%{http_code}\' https://localhost:23443/candlepin/status')
     assert status.succeeded
