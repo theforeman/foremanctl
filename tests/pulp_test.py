425e74252c4f545659e5ef306f2d8141646ebd86
Evgeni Golov <evgeni@golov.de>
2024-09-25 13:27:48 +0200

    run pulp as a quadlet

--- tests/pulp_test.py
+++ tests/pulp_test.py
@@ -5,7 +5,6 @@ PULP_HOST = 'localhost'
 PULP_PORT = 8080
 
 
-@pytest.mark.xfail(reason="Need to convert setup to a systemd service")
 def test_pulp_service(host):
     pulp = host.service("pulp")
     assert pulp.is_running
