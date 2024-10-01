882db0cd88f37312a9eda7e21c0b6f2399c8f554
Evgeni Golov <evgeni@golov.de>
2024-10-01 09:16:11 +0200

    split pulp status test into fixtures and many tests

--- tests/pulp_test.py
+++ tests/pulp_test.py
@@ -7,6 +7,16 @@ PULP_HOST = 'localhost'
 PULP_PORT = 8080
 
 
+@pytest.fixture(scope="module")
+def pulp_status_curl(host):
+    return host.run(f"curl -k -s -w '%{{stderr}}%{{http_code}}' http://{PULP_HOST}:{PULP_PORT}/pulp/api/v3/status/")
+
+
+@pytest.fixture(scope="module")
+def pulp_status(pulp_status_curl):
+    return json.loads(pulp_status_curl.stdout)
+
+
 def test_pulp_service(host):
     pulp = host.service("pulp")
     assert pulp.is_running
@@ -18,14 +28,26 @@ def test_pulp_port(host):
     assert pulp.port(PULP_PORT).is_reachable
 
 
-def test_pulp_status(host):
-    status = host.run(f"curl -k -s -w '%{{stderr}}%{{http_code}}' http://{PULP_HOST}:{PULP_PORT}/pulp/api/v3/status/")
-    assert status.succeeded
-    assert status.stderr == '200'
+def test_pulp_status(pulp_status_curl):
+    assert pulp_status_curl.succeeded
+    assert pulp_status_curl.stderr == '200'
 
-    status_json = json.loads(status.stdout)
-    assert status_json['database_connection']['connected']
-    assert status_json['redis_connection']['connected']
-    assert status_json['online_api_apps']
-    assert status_json['online_content_apps']
-    assert status_json['online_workers']
+
+def test_pulp_status_database_connection(pulp_status):
+    assert pulp_status['database_connection']['connected']
+
+
+def test_pulp_status_redis_connection(pulp_status):
+    assert pulp_status['redis_connection']['connected']
+
+
+def test_pulp_status_api(pulp_status):
+    assert pulp_status['online_api_apps']
+
+
+def test_pulp_status_content(pulp_status):
+    assert pulp_status['online_content_apps']
+
+
+def test_pulp_status_workers(pulp_status):
+    assert pulp_status['online_workers']
