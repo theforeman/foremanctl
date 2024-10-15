e840f5a70105bcb744a7b10ae0125e73786acc55
akumari <akumari@redhat.com>
2024-10-15 15:39:45 +0530

    add pulp services

--- tests/pulp_test.py
+++ tests/pulp_test.py
@@ -1,11 +1,11 @@
 import json
-
 import pytest
 
 
 PULP_HOST = 'localhost'
 PULP_PORT = 8080
-
+PULP_API_PORT = 24817
+PULP_CONTENT_PORT = 24816
 
 @pytest.fixture(scope="module")
 def pulp_status_curl(host):
@@ -22,16 +22,37 @@ def test_pulp_service(host):
     assert pulp.is_running
     assert pulp.is_enabled
 
+def test_pulp_api_service(host):
+    pulp_api = host.service("pulp-api")
+    assert pulp_api.is_running
+    assert pulp_api.is_enabled
+
+def test_pulp_content_service(host):
+    pulp_content = host.service("pulp-content")
+    assert pulp_content.is_running
+    assert pulp_content.is_enabled
+
+def test_pulp_worker_services(host):
+    for i in range(1, 3):
+        pulp_worker = host.service(f"pulp-worker@{i}")
+        assert pulp_worker.is_running
+        assert pulp_worker.is_enabled
 
 def test_pulp_port(host):
     pulp = host.addr(PULP_HOST)
     assert pulp.port(PULP_PORT).is_reachable
 
+def test_pulp_api_port(host):
+    pulp_api = host.addr(PULP_HOST)
+    assert pulp_api.port(PULP_API_PORT).is_reachable
+
+def test_pulp_content_port(host):
+    pulp_content = host.addr(PULP_HOST)
+    assert pulp_content.port(PULP_CONTENT_PORT).is_reachable
 
 def test_pulp_status(pulp_status_curl):
     assert pulp_status_curl.succeeded
     assert pulp_status_curl.stderr == '200'
-
 
 def test_pulp_status_database_connection(pulp_status):
     assert pulp_status['database_connection']['connected']
