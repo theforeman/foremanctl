theforeman/foremanctl

commit 371727b767dd7b012566138069ea08e54b00906b

add WantedBy=default.target to all containers

roles/foreman/tasks/main.yaml
@@ -56,6 +56,10 @@
       - 'foreman-client-key,type=mount,target=/etc/foreman/client_key.pem'
     env:
       SEED_ADMIN_PASSWORD: changeme
+    quadlet_options:
+      - |
+        [Install]
+        WantedBy=default.target
 
 - name: Deploy Dynflow Container
   containers.podman.podman_container:

roles/foreman-proxy/tasks/main.yaml
@@ -62,6 +62,10 @@
       - 'foreman-proxy-ca-cert,type=mount,target=/etc/foreman-proxy/ssl_ca.pem'
       - 'foreman-proxy-server-cert,type=mount,target=/etc/foreman-proxy/ssl_cert.pem'
       - 'foreman-proxy-server-key,type=mount,target=/etc/foreman-proxy/ssl_key.pem'
+    quadlet_options:
+      - |
+        [Install]
+        WantedBy=default.target
 
 - name: Run daemon reload to make Quadlet create the service files
   ansible.builtin.systemd:

roles/pulp/tasks/main.yaml
@@ -24,6 +24,10 @@
     volumes: "{{ pulp_volumes }}"
     secrets:
       - 'pulp-settings-py,type=mount,target=/etc/pulp/settings.py'
+    quadlet_options:
+      - |
+        [Install]
+        WantedBy=default.target
 
 - name: Run daemon reload to make Quadlet create the service files
   ansible.builtin.systemd:

roles/candlepin/tasks/main.yml
@@ -72,6 +72,10 @@
     volumes:
       - /var/log/candlepin:/var/log/candlepin:Z
       - /var/log/tomcat:/var/log/tomcat:Z
+    quadlet_options:
+      - |
+        [Install]
+        WantedBy=default.target
 
 - name: Run daemon reload to make Quadlet create the service files
   ansible.builtin.systemd:

commit b5f12b00c5f14903952e0a9f9ee929fedb425928

also mark dynflow containers as auto-started
Fixes: 371727b767dd7b012566138069ea08e54b00906b

roles/foreman/tasks/main.yaml
@@ -87,6 +87,20 @@
       DYNFLOW_REDIS_URL: "redis://localhost:6379/6"
       REDIS_PROVIDER: "DYNFLOW_REDIS_URL"
     command: "/usr/libexec/foreman/sidekiq-selinux -e production -r /usr/share/foreman/extras/dynflow-sidekiq.rb -C /etc/foreman/dynflow/%i.yml"
+    quadlet_options:
+      - |
+        [Install]
+        WantedBy=default.target
+
+- name: Create Dynflow Container instances
+  ansible.builtin.file:
+    state: link
+    src: "/etc/containers/systemd/dynflow-sidekiq@.container"
+    dest: "/etc/containers/systemd/dynflow-sidekiq@{{ item }}.container"
+  loop:
+    - orchestrator
+    - worker
+    - worker-hosts-queue
 
 - name: Run daemon reload to make Quadlet create the service files
   ansible.builtin.systemd:

tests/foreman_test.py
@@ -49,8 +49,15 @@ def test_katello_services_status(foreman_status, katello_service):
     assert foreman_status['results']['katello']['services'][katello_service]['status'] == 'ok'
 
 
-@pytest.mark.parametrize("dynflow_service", ['orchestrator', 'worker', 'worker-hosts-queue'])
-def test_foreman_dynflow_service(host, dynflow_service):
-    service = host.service(f"dynflow-sidekiq@{dynflow_service}")
+@pytest.mark.parametrize("dynflow_instance", ['orchestrator', 'worker', 'worker-hosts-queue'])
+def test_foreman_dynflow_container_instances(host, dynflow_instance):
+    file = host.file(f"/etc/containers/systemd/dynflow-sidekiq@{dynflow_instance}.container")
+    assert file.exists
+    assert file.is_symlink
+
+
+@pytest.mark.parametrize("dynflow_instance", ['orchestrator', 'worker', 'worker-hosts-queue'])
+def test_foreman_dynflow_service_instances(host, dynflow_instance):
+    service = host.service(f"dynflow-sidekiq@{dynflow_instance}")
     assert service.is_running
     assert service.is_enabled