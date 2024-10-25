81a05ab2e554ea02200e55d0380559d1dc5b040e
Evgeni Golov <evgeni@golov.de>
2024-10-25 13:20:32 +0200

    use the api to create a set of reusable fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+import uuid
+
+import apypie
+import paramiko
 import pytest
 import testinfra
 
@@ -5,3 +9,68 @@ import testinfra
 @pytest.fixture(scope="module")
 def server():
     yield testinfra.get_host('paramiko://quadlet', sudo=True, ssh_config='./.vagrant/ssh-config')
+
+
+@pytest.fixture(scope="module")
+def ssh_config():
+    config = paramiko.SSHConfig.from_path('./.vagrant/ssh-config')
+    return config.lookup('quadlet')
+
+
+@pytest.fixture(scope="module")
+def foremanapi(ssh_config):
+    return apypie.ForemanApi(
+        uri=f'https://{ssh_config['hostname']}',
+        username='admin',
+        password='changeme',
+        verify_ssl=False,
+    )
+
+@pytest.fixture
+def organization(foremanapi):
+    org = foremanapi.create('organizations', {'name': str(uuid.uuid4())})
+    yield org
+    foremanapi.delete('organizations', org)
+
+@pytest.fixture
+def product(organization, foremanapi):
+    prod = foremanapi.create('products', {'name': str(uuid.uuid4()), 'organization_id': organization['id']})
+    yield prod
+    foremanapi.delete('products', prod)
+
+@pytest.fixture
+def yum_repository(product, organization, foremanapi):
+    repo = foremanapi.create('repositories', {'name': str(uuid.uuid4()), 'product_id': product['id'], 'content_type': 'yum', 'url': 'https://fixtures.pulpproject.org/rpm-no-comps/'})
+    yield repo
+    foremanapi.delete('repositories', repo)
+
+@pytest.fixture
+def file_repository(product, organization, foremanapi):
+    repo = foremanapi.create('repositories', {'name': str(uuid.uuid4()), 'product_id': product['id'], 'content_type': 'file', 'url': 'https://fixtures.pulpproject.org/file/'})
+    yield repo
+    foremanapi.delete('repositories', repo)
+
+@pytest.fixture
+def container_repository(product, organization, foremanapi):
+    repo = foremanapi.create('repositories', {'name': str(uuid.uuid4()), 'product_id': product['id'], 'content_type': 'docker', 'url': 'https://quay.io/', 'docker_upstream_name': 'foreman/busybox-test'})
+    yield repo
+    foremanapi.delete('repositories', repo)
+
+@pytest.fixture
+def lifecycle_environment(organization, foremanapi):
+    library = foremanapi.list('lifecycle_environments', 'name=Library', {'organization_id': organization['id']})[0]
+    lce = foremanapi.create('lifecycle_environments', {'name': str(uuid.uuid4()), 'organization_id': organization['id'], 'prior_id': library['id']})
+    yield lce
+    foremanapi.delete('lifecycle_environments', lce)
+
+@pytest.fixture
+def content_view(organization, foremanapi):
+    cv = foremanapi.create('content_views', {'name': str(uuid.uuid4()), 'organization_id': organization['id']})
+    yield cv
+    foremanapi.delete('content_views', cv)
+
+@pytest.fixture
+def activation_key(organization, foremanapi):
+    ak = foremanapi.create('activation_keys', {'name': str(uuid.uuid4()), 'organization_id': organization['id']})
+    yield ak
+    foremanapi.delete('activation_keys', ak)
