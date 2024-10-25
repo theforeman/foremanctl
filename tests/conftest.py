aa775544b9c19c9dc8e6b12dfe3010df61ce1e46
Evgeni Golov <evgeni@golov.de>
2024-10-25 13:20:32 +0200

    use the api to create a set of reusable fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import uuid
+
+import apypie
+import paramiko
+import pytest
+
+
+@pytest.fixture(scope="module")
+def ssh_config():
+    config = paramiko.SSHConfig.from_path('./.vagrant/ssh-config')
+    return config.lookup('quadlet')
+
+
+@pytest.fixture(scope="module")
+def foremanapi(ssh_config):
+    return apypie.ForemanApi(
+        uri=f'https://{ssh_config['hostname']}',
+        username='admin',
+        password='changeme',
+        verify_ssl=False,
+    )
+
+@pytest.fixture
+def organization(foremanapi):
+    org = foremanapi.create('organizations', {'name': str(uuid.uuid4())})
+    yield org
+    foremanapi.delete('organizations', org)
+
+@pytest.fixture
+def product(organization, foremanapi):
+    prod = foremanapi.create('products', {'name': str(uuid.uuid4()), 'organization_id': organization['id']})
+    yield prod
+    foremanapi.delete('products', prod)
+
+@pytest.fixture
+def repository(product, organization, foremanapi):
+    repo = foremanapi.create('repositories', {'name': str(uuid.uuid4()), 'product_id': product['id'], 'content_type': 'yum'})
+    yield repo
+    foremanapi.delete('repositories', repo)
---
+++ tests/foreman_api_test.py
@@ -0,0 +1,8 @@
+def test_foreman_organization(organization):
+    assert organization
+
+def test_foreman_product(product):
+    assert product
+
+def test_foreman_repository(repository):
+    assert repository
