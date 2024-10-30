2a77e2e7fe93953720e2df79306c50f7809103b2
Evgeni Golov <evgeni@golov.de>
2024-10-30 09:40:06 +0100

    yum/file

--- tests/foreman_api_test.py
+++ tests/foreman_api_test.py
@@ -1,8 +1,28 @@
+import urllib.parse
+
+import requests
+
+
+def _repo_url(repo, ssh_config):
+    return urllib.parse.urlunparse(urllib.parse.urlparse(repo['full_path'])._replace(netloc=ssh_config['hostname']))
+
+
 def test_foreman_organization(organization):
     assert organization
 
 def test_foreman_product(product):
     assert product
 
-def test_foreman_repository(repository):
-    assert repository
+def test_foreman_yum_repository(yum_repository, foremanapi, ssh_config):
+    assert yum_repository
+    foremanapi.resource_action('repositories', 'sync', {'id': yum_repository['id']})
+    repo_url = _repo_url(yum_repository, ssh_config)
+    assert requests.get(f'{repo_url}/repodata/repomd.xml', verify=False)
+    assert requests.get(f'{repo_url}/Packages/b/bear-4.1-1.noarch.rpm', verify=False)
+
+
+def test_foreman_file_repository(file_repository, foremanapi, ssh_config):
+    assert file_repository
+    foremanapi.resource_action('repositories', 'sync', {'id': file_repository['id']})
+    repo_url = _repo_url(file_repository, ssh_config)
+    assert requests.get(f'{repo_url}/1.iso', verify=False)
