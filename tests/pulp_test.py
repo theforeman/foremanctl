ced9c95db7a9488ecc368311f9ca6a6e222cf62e
Evgeni Golov <evgeni@golov.de>
2024-11-04 17:30:52 +0100

    use right ports for pulp in http config

--- tests/pulp_test.py
+++ tests/pulp_test.py
@@ -1,26 +1,17 @@
 import json
 import pytest
 
-
 PULP_HOST = 'localhost'
-PULP_PORT = 8080
 PULP_API_PORT = 24817
 PULP_CONTENT_PORT = 24816
 
 @pytest.fixture(scope="module")
 def pulp_status_curl(host):
-    return host.run(f"curl -k -s -w '%{{stderr}}%{{http_code}}' http://{PULP_HOST}:{PULP_PORT}/pulp/api/v3/status/")
-
+    return host.run(f"curl -k -s -w '%{{stderr}}%{{http_code}}' http://{PULP_HOST}:{PULP_API_PORT}/pulp/api/v3/status/")
 
 @pytest.fixture(scope="module")
 def pulp_status(pulp_status_curl):
     return json.loads(pulp_status_curl.stdout)
-
-
-def test_pulp_service(host):
-    pulp = host.service("pulp")
-    assert pulp.is_running
-    assert pulp.is_enabled
 
 def test_pulp_api_service(host):
     pulp_api = host.service("pulp-api")
@@ -32,15 +23,10 @@ def test_pulp_content_service(host):
     assert pulp_content.is_running
     assert pulp_content.is_enabled
 
-def test_pulp_worker_services(host):
-    for i in range(1, 3):
-        pulp_worker = host.service(f"pulp-worker@{i}")
-        assert pulp_worker.is_running
-        assert pulp_worker.is_enabled
-
-def test_pulp_port(host):
-    pulp = host.addr(PULP_HOST)
-    assert pulp.port(PULP_PORT).is_reachable
+def test_pulp_worker_service(host):
+    pulp_worker = host.service("pulp-worker")
+    assert pulp_worker.is_running
+    assert pulp_worker.is_enabled
 
 def test_pulp_api_port(host):
     pulp_api = host.addr(PULP_HOST)
@@ -49,10 +35,6 @@ def test_pulp_api_port(host):
 def test_pulp_content_port(host):
     pulp_content = host.addr(PULP_HOST)
     assert pulp_content.port(PULP_CONTENT_PORT).is_reachable
-
-def test_pulp_status(pulp_status_curl):
-    assert pulp_status_curl.succeeded
-    assert pulp_status_curl.stderr == '200'
 
 def test_pulp_status_database_connection(pulp_status):
     assert pulp_status['database_connection']['connected']
@@ -75,6 +57,9 @@ def test_pulp_status_workers(pulp_status):
 
 @pytest.mark.xfail(reason='password auth is deactivated when we use cert auth')
 def test_pulp_admin_auth(host):
-    cmd = host.run(f"curl --silent --write-out '%{{stderr}}%{{http_code}}' --user admin:CHANGEME http://{PULP_HOST}:{PULP_PORT}/pulp/api/v3/users/")
+    cmd = host.run(f"curl --silent --write-out '%{{stderr}}%{{http_code}}' --user admin:CHANGEME http://{PULP_HOST}:{PULP_API_PORT}/pulp/api/v3/users/")
     assert cmd.succeeded
     assert cmd.stderr == '200'
+
+def test_pulp_volumes(host):
+    assert host.file("/var/lib/pulp").is_directory
