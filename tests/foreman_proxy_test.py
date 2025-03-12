5588299abd0137477c06eca6591bfb9684f4f86f
Eric D. Helms <ericdhelms@gmail.com>
2025-03-12 17:07:33 -0400

    Split installation into three distinct phases
    Signed-off-by: Eric D. Helms <ericdhelms@gmail.com>

--- tests/foreman_proxy_test.py
+++ tests/foreman_proxy_test.py
@@ -7,6 +7,32 @@ FOREMAN_PROXY_HOST = 'localhost'
 FOREMAN_PROXY_PORT = 9090
 
 
+@pytest.fixture(scope="module")
+def foreman_proxy_version_curl(server):
+    return server.run(f"curl -k -s -w '%{{stderr}}%{{http_code}}' https://{FOREMAN_PROXY_HOST}:{FOREMAN_PROXY_PORT}/version")
+
+
+@pytest.fixture(scope="module")
+def foreman_proxy_features_curl(server, certificates):
+    return server.run(f"curl -k -s -w '%{{stderr}}%{{http_code}}' --cert {certificates['client_certificate']} --key {certificates['client_key']} https://{FOREMAN_PROXY_HOST}:{FOREMAN_PROXY_PORT}/v2/features")
+
+
+def test_foreman_proxy_service(server):
+    foreman_proxy = server.service("foreman-proxy")
+    assert foreman_proxy.is_running
+    assert foreman_proxy.is_enabled
+
+
 def test_foreman_proxy_port(server):
     foreman_proxy = server.addr(FOREMAN_PROXY_HOST)
-    assert not foreman_proxy.port(FOREMAN_PROXY_PORT).is_reachable
+    assert foreman_proxy.port(FOREMAN_PROXY_PORT).is_reachable
+
+
+def test_foreman_proxy_version(foreman_proxy_version_curl):
+    assert foreman_proxy_version_curl.succeeded
+    assert foreman_proxy_version_curl.stderr == '200'
+
+
+def test_foreman_proxy_features(foreman_proxy_features_curl):
+    assert foreman_proxy_features_curl.succeeded
+    assert foreman_proxy_features_curl.stderr == '200'
