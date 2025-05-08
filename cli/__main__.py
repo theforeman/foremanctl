theforeman/foreman-quadlet

Prototyping CLI based on Obsah
Signed-off-by: Eric D. Helms <ericdhelms@gmail.com>

cli/__main__.py
@@ -1,0 +1,6 @@
+"""
+Main module file. This allows you to run python -m obal
+"""
+if __name__ == "__main__":
+    from cli import main  # pylint:disable=no-name-in-module
+    main()

theforeman/foremanctl

cli/__main__.py
@@ -1,0 +1,6 @@
+"""
+Main module file. This allows you to run python -m cli
+"""
+if __name__ == "__main__":
+    from cli import main  # pylint:disable=no-name-in-module
+    main()