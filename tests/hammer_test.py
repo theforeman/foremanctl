52248fb71df6fce6e9f800baa36baf7384b42602
Evgeni Golov <evgeni@golov.de>
2025-09-23 11:55:18 +0200

    skip hammer on Debian

--- tests/hammer_test.py
+++ tests/hammer_test.py
@@ -1,7 +1,14 @@
+import pytest
+
+
 def test_hammer_ping(server):
+    if server.system_info.distribution == 'debian':
+        pytest.xfail('Hammer is not properly set up on Debian yet')
     hammer = server.run("hammer ping")
     assert hammer.succeeded
 
 def test_hammer_organizations_list(server):
+    if server.system_info.distribution == 'debian':
+        pytest.xfail('Hammer is not properly set up on Debian yet')
     hammer = server.run("hammer organization list")
     assert hammer.succeeded
