f1abab20d47e76f22fc87f3308268893c671b1b5
Evgeni Golov <evgeni@golov.de>
2025-09-23 11:55:18 +0200

    skip hammer on Debian

--- tests/foreman_compute_resources_test.py
+++ tests/foreman_compute_resources_test.py
@@ -5,6 +5,8 @@ FOREMAN_PORT = 3000
 
 @pytest.mark.parametrize("compute_resource", ['AzureRm', 'EC2', 'GCE', 'Libvirt', 'Openstack', 'Vmware'])
 def test_foreman_compute_resources(server, compute_resource):
+    if server.system_info.distribution == 'debian':
+        pytest.xfail('Hammer is not properly set up on Debian yet')
     hammer = server.run("hammer compute-resource create --help | grep provider")
     assert hammer.succeeded
     assert compute_resource in hammer.stdout
