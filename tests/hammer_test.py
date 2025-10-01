4c393915c7b7dc5da2a041041eb1d5a4329d8c94
Arvind Jangir <ajangir@ajangir-thinkpadt14gen5.punetw6.csb>
2025-10-01 12:56:55 +0530

    Add tests and fixed ansible lint

--- tests/hammer_test.py
+++ tests/hammer_test.py
@@ -1,3 +1,7 @@
 def test_hammer_ping(server):
     hammer = server.run("hammer ping")
     assert hammer.succeeded
+
+def test_hammer_organizations_list(server):
+    hammer = server.run("hammer organization list")
+    assert hammer.succeeded
