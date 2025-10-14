453df3eee72e7cb1c1318eaaed61d150f2dcd70e
Leos Stejskal <lstejska@redhat.com>
2025-10-14 09:23:49 +0200

    Use scram-sha-256 for password encryption

--- tests/postgresql_test.py
+++ tests/postgresql_test.py
@@ -1,3 +1,5 @@
+import csv
+
 def test_postgresql_service(server):
     postgresql = server.service("postgresql")
     assert postgresql.is_running
@@ -20,3 +22,13 @@ def test_postgresql_users(server):
     assert "foreman" in result.stdout
     assert "candlepin" in result.stdout
     assert "pulp" in result.stdout
+
+def test_postgresql_password_encryption(server):
+    result = server.run("podman exec postgresql psql -U postgres -c 'SHOW password_encryption'")
+    assert "scram-sha-256" in result.stdout
+
+    result = server.run("echo 'COPY (select * from pg_shadow) TO STDOUT (FORMAT CSV);' | podman exec -i postgresql psql -U postgres")
+
+    reader = csv.reader(result.stdout.splitlines())
+    for row in reader:
+        assert ("SCRAM-SHA-256" in row[6])
