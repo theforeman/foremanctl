bc2641d3c38bc09be1e157e36c11ee6d37769d74
Leos Stejskal <lstejska@redhat.com>
2025-10-23 14:41:46 +0200

    Setup Foreman Proxy
    foremanctl setup-foreman-proxy command for deploying Foreman Proxy.

---
+++ tests/foreman_proxy_test.py
@@ -0,0 +1,15 @@
+FOREMAN_PROXY_PORT = 8443
+
+def test_foreman_proxy_features(server, certificates, server_fqdn):
+    cmd = server.run(f"curl --cacert {certificates['ca_certificate']} --silent https://{server_fqdn}:{FOREMAN_PROXY_PORT}/features")
+    assert cmd.succeeded
+    assert "logs" in cmd.stdout
+
+def test_foreman_proxy_service(server):
+    foreman_proxy = server.service("foreman-proxy")
+    assert foreman_proxy.is_running
+    assert foreman_proxy.is_enabled
+
+def test_foreman_proxy_port(server):
+    foreman_proxy = server.addr('localhost')
+    assert foreman_proxy.port(FOREMAN_PROXY_PORT).is_reachable
