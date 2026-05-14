1bf96678c2864382877d7d8bd67096aa01a06329
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2026-05-14 22:32:02 +0200

    Remove unused FOREMAN_{HOST,PORT} constants

--- tests/foreman_compute_resources_test.py
+++ tests/foreman_compute_resources_test.py
@@ -1,8 +1,5 @@
 import pytest
 
-FOREMAN_HOST = 'localhost'
-FOREMAN_PORT = 3000
-
 
 @pytest.mark.parametrize("compute_resource", ['AzureRm', 'EC2', 'GCE', 'Libvirt', 'Openstack', 'Vmware'])
 def test_foreman_compute_resources(server, compute_resource):
