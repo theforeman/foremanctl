e0722a49e893f90e255251ce4edc660e5d445a0e
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2026-05-15 20:56:58 +0200

    Correctly assert foreman-proxy features

--- tests/feature/foreman-proxy/base_test.py
+++ tests/feature/foreman-proxy/base_test.py
@@ -23,8 +23,17 @@ def test_foreman_proxy_features(server, certificates, server_fqdn, enabled_featu
     assert cmd.succeeded
     features = json.loads(cmd.stdout)
     assert "logs" in features
-    assert "script" in features
-    assert "dynflow" in features
+
+    if "remote-execution" in enabled_features:
+        assert "script" in features
+    else:
+        assert "script" not in features
+
+    if "dynflow" in enabled_features:
+        assert "dynflow" in features
+    else:
+        assert "dynflow" not in features
+
     if 'bmc' in enabled_features:
         assert "bmc" in features
     else:
