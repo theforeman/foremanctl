49bfbcac0f55d414124902374372763220628997
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2026-05-15 16:13:41 +0200

    Only test for azure-rm and google features when enabled

--- tests/feature/foreman/compute_resources_test.py
+++ tests/feature/foreman/compute_resources_test.py
@@ -1,9 +1,25 @@
 import pytest
 
 
-@pytest.mark.parametrize("compute_resource", ['AzureRm', 'EC2', 'GCE', 'Libvirt', 'Openstack', 'Vmware'])
-def test_foreman_compute_resources(foremanapi, compute_resource):
+# TODO: Foreman really should have a dedicated API endpoint to expose this info
+@pytest.fixture
+def provider_description(foremanapi):
     create = foremanapi.resource('compute_resources').action('create')
     compute_resource_param = [param for param in create.params if param.name == 'compute_resource'][0]
     provider = [param for param in compute_resource_param.params if param.name == 'provider'][0]
-    assert compute_resource in provider.description
+    return provider.description
+
+
+@pytest.mark.parametrize("compute_resource", ['EC2', 'Libvirt', 'Openstack', 'Vmware'])
+def test_foreman_compute_resources_built_in(provider_description, compute_resource):
+    assert compute_resource in provider_description
+
+
+@pytest.mark.feature('azure-rm')
+def test_foreman_compute_resources_azure_rm(provider_description):
+    assert 'AzureRm' in provider_description
+
+
+@pytest.mark.feature('google')
+def test_foreman_compute_resources_google(provider_description):
+    assert 'GCE' in provider_description
