715b2e04e1050f0554b093e486efcbd80c15d124
akumari <akumari@redhat.com>
2026-06-02 15:04:03 +0530

    Fix DHCP KEA settings

--- tests/foreman_proxy_test.py
+++ tests/foreman_proxy_test.py
@@ -29,7 +29,7 @@ def test_foreman_proxy_features(server, certificates, server_fqdn, enabled_featu
         assert "bmc" in features
     else:
         assert "bmc" not in features
-    if 'dhcp-kea-external' in enabled_features:
+    if 'dhcp' in enabled_features:
         assert "dhcp" in features
     else:
         assert "dhcp" not in features
@@ -75,24 +75,24 @@ def test_bmc_default_provider(proxy_v2_features):
     assert settings.get('bmc_default_provider') == 'ipmitool'
 
 
-@pytest.mark.feature('dhcp-kea-external')
-def test_dhcp_kea_feature_present(proxy_v2_features):
+@pytest.mark.feature('dhcp')
+def test_dhcp_feature_present(proxy_v2_features):
     assert 'dhcp' in proxy_v2_features
 
 
-@pytest.mark.feature('dhcp-kea-external')
-def test_dhcp_kea_provider(proxy_v2_features):
-    assert 'dhcp' in proxy_v2_features
-    settings = proxy_v2_features['dhcp'].get('settings', {})
-    assert settings.get('use_provider') == 'dhcp_kea_api'
-
-
-@pytest.mark.feature('dhcp-kea-external')
-def test_dhcp_kea_server_settings(proxy_v2_features):
+@pytest.mark.feature('dhcp')
+def test_dhcp_provider(proxy_v2_features):
     assert 'dhcp' in proxy_v2_features
     settings = proxy_v2_features['dhcp'].get('settings', {})
-    assert 'server' in settings
-    assert 'kea_url' in settings
-    assert settings['kea_url'].endswith(':8000')
-    assert 'kea_subnet' in settings
-    assert '/' in settings['kea_subnet']
+    assert settings.get('use_provider') == 'dhcp_kea'
+
+
+@pytest.mark.feature('dhcp')
+def test_dhcp_kea_api_settings(proxy_v2_features):
+    assert 'dhcp' in proxy_v2_features
+    settings = proxy_v2_features['dhcp'].get('settings', {})
+    assert 'kea_api_url' in settings
+    assert settings['kea_api_url'].endswith(':8000')
+    if settings.get('managed_subnets'):
+        assert isinstance(settings['managed_subnets'], list)
+        assert all('/' in subnet for subnet in settings['managed_subnets'])
