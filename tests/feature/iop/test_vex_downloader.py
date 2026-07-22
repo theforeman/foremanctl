012420606c6ca8b1fbf867b724a8d1d10de2df7f
Petr Freyburg <pfreybur@redhat.com>
2026-07-22 14:10:03 +0200

    IoP vex: fix linting

--- tests/feature/iop/test_vex_downloader.py
+++ tests/feature/iop/test_vex_downloader.py
@@ -54,8 +54,10 @@ def test_vex_download_path_enabled(server):
     assert path.is_enabled
     assert path.is_running
 
+
 def test_vex_download_folder_exists(server):
     assert server.file("/var/www/html/pub/iop/data/csaf/v2/vex/").is_directory
+
 
 def test_vex_download_archive_latest_exists(server):
     file = server.file("/var/www/html/pub/iop/data/csaf/v2/vex/archive_latest.txt")
