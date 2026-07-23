3aa2e620bd2dc432c93e017db5c1d21349f54816
Arvind Jangir <ajangir@redhat.com>
2026-07-23 18:57:15 +0530

    add new role for oauth creds

--- tests/flavor/foreman-proxy-content/oauth_test.py
+++ tests/flavor/foreman-proxy-content/oauth_test.py
@@ -5,13 +5,13 @@ OAUTH_FILES = [
     'foreman-oauth-consumer-secret',
 ]
 
-CERTS_DIR = '/var/lib/foremanctl/certs'
+OAUTH_DIR = '/var/lib/foremanctl/certs/oauth'
 
 
 @pytest.mark.parametrize("oauth_file", OAUTH_FILES)
 def test_oauth_credentials_extracted_from_bundle(server, oauth_file):
     """Proxy deploy must extract OAuth credentials from the certificate bundle."""
-    oauth_path = f'{CERTS_DIR}/{oauth_file}'
+    oauth_path = f'{OAUTH_DIR}/{oauth_file}'
     oauth = server.file(oauth_path)
     assert oauth.exists, f'{oauth_path} was not extracted from the certificate bundle'
     assert oauth.size > 0
