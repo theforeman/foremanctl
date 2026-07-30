ed16c459e5ae268d9cc8bd3839465ffe84d41b66
Aiden Fine <aiden03fine@gmail.com>
2026-07-30 12:32:36 -0400

    add support for subtests

--- tests/feature/iop/test_target_lifecycle.py
+++ tests/feature/iop/test_target_lifecycle.py
@@ -1,4 +1,4 @@
-def test_iop_services_part_of_foreman_target(server):
+def test_iop_services_part_of_foreman_target(server, subtests):
     result = server.run(
         "systemctl list-units 'iop-core-*' 'iop-service-*'"
         " --type=service --no-legend --all --plain"
@@ -7,11 +7,9 @@ def test_iop_services_part_of_foreman_target(server):
     services = [line.split()[0] for line in result.stdout.strip().splitlines()]
     assert services, "No IOP services found"
 
-    missing = []
     for service_name in services:
         service = server.service(service_name)
         if service.systemd_properties.get("TriggeredBy"):
             continue
-        assert "foreman.target" in service.systemd_properties.get("PartOf")
-
-    assert not missing, f"IOP services not PartOf foreman.target: {missing}"
+        with subtests.test(service_name):
+            assert "foreman.target" in service.systemd_properties.get("PartOf")
