892d8d88b4a4c3b1151eb6e4c943f6d260886e1a
Aiden Fine <aiden03fine@gmail.com>
2026-07-31 10:24:18 -0400

    add subtest back to test_ios_services

--- tests/feature/iop/test_target_lifecycle.py
+++ tests/feature/iop/test_target_lifecycle.py
@@ -1,4 +1,4 @@
-def test_iop_services_part_of_foreman_target(server):
+def test_iop_services_part_of_foreman_target(server, subtests):
     result = server.run(
         "systemctl list-units 'iop-core-*' 'iop-service-*'"
         " --type=service --no-legend --all --plain"
@@ -11,4 +11,5 @@ def test_iop_services_part_of_foreman_target(server):
         service = server.service(service_name)
         if service.systemd_properties.get("TriggeredBy"):
             continue
-        assert "foreman.target" in service.systemd_properties.get("PartOf")
+        with subtests.test(service_name):
+            assert "foreman.target" in service.systemd_properties.get("PartOf")
